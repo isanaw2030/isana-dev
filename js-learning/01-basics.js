16a434d6fe8731a7d5636946a3e50c615a78549c
isanaw2030 <isaacwafula662@gmail.com>
2026-09-18 14:44:39 +0300

    day 1: JS basics - shop product and price

---
+++ js-learning/01-basics.js
@@ -0,0 +1,10 @@
+let product = "T-shirt";
+let price = 1500;
+const shopName = "ISANA Shop";
+
+console.log("Shop: " + shopName);
+console.log("Product: " + product);
+console.log("Price: " + price);
+let total = price * 2;
+console.log(`2 T-Shirts: KES ${total}`);
+
